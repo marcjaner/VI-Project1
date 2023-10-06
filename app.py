b87fc93bcdddf41fa7a784521c0b78fd06b1d942
Marcufli <marcjanerferrer@gmail.com>
2023-10-06 13:20:44 +0200

    initial files

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from typing_extensions import TypeAlias
+from streamlit_option_menu import option_menu
+import pandas as pd
+from vega_datasets import data
+import pandas as pd
+import numpy as np
+import altair as alt
+
+
+def main() -> None:
+    st.title('Project 1')
+    st.write("Marc Janer")
+
+
+main()
